49efefe5b3c3d2a7a9d669b4699174caa7cc3403
GATA5 <Tangjing.Gan@coop.ch>
2023-11-08 14:34:42 +0100

    Create Title, Author, ISBN with getter and setter

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,9 +1,41 @@
 public class Book {
+    private String title;
+    private String author;
+    private int isbn;
 
+    Book() {
 
+    }
 
+    public Book(String title, String author, int isbn){
+        this.title = title
+        this.author = author;
+        this.isbn = isbn;
+    }
 
+    public int getTitle() {
+        return title;
+    }
 
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getisbn() {
+        return isbn;
+    }
+
+    public void setAge(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 
 
 
